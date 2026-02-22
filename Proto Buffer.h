db1423f4620fc8df122823b11301951145462140
narekkh19 <narekkh19@NAREKKHOREN005E>
2026-02-22 16:57:07 +0400

    Add project files.

---
+++ Proto Buffer.h
@@ -0,0 +1,11 @@
+﻿// Proto Buffer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+// TODO: Reference additional headers your program requires here.
